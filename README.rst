120fda6946240921e800112c0079a4c76bd21e8d
faassen <faassen@startifact.com>
2016-06-09 16:29:45 +0200

    Initial project skeleton.

---
+++ README.rst
@@ -0,0 +1,5 @@
+Proxima: Async reverse HTTP proxy
+=================================
+
+Proxima is an asynchronous reverse HTTP proxy for Python 3 built on
+aiohttp.
---
+++ setup.py
@@ -0,0 +1,39 @@
+import io
+from setuptools import setup, find_packages
+
+long_description = '\n'.join((
+    io.open('README.rst', encoding='utf-8').read(),
+    io.open('CHANGES.txt', encoding='utf-8').read()
+))
+
+setup(
+    name='proxima',
+    version='0.1.dev0',
+    description="Asynchronous reverse HTTP proxy",
+    long_description=long_description,
+    author="Martijn Faassen",
+    author_email="faassen@startifact.com",
+    url='http://proxima.readthedocs.org',
+    license="BSD",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords="configuration",
+    install_requires=[
+        'setuptools'
+    ],
+    extras_require=dict(
+        test=[
+            'pytest >= 2.5.2',
+            'pytest-asyncio',
+            'py >= 1.4.20',
+            'pytest-cov',
+            'pytest-remove-stale-bytecode',
+        ],
+    ),
+)
